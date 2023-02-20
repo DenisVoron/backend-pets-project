b7cbeeaed179280abe95d9994a3feb5d7fa8ae5a
DenisVoron <Denis.voronko@gmail.com>
2023-02-20 23:35:51 +0200

    add fix3

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,12 @@ const auth = require('./auth');
 const news = require('./news');
 const services = require('./services');
 const notices = require('./notices');
+const pets = require('./pets');
 
 module.exports = {
   auth,
   news,
   services,
   notices,
+  pets,
 };
--- src/routes/api/pets.js
+++ src/routes/api/pets.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const ctrl = require('../../controllers/pets');
+const { pets: ctrl } = require('../../controllers');
 
 const { validation, authentication, ctrlWrapper, upload } = require('../../middlewares');
 
 const { joiSchema } = require('../../models/pet');
 
 router.get('/pet/current', authentication, ctrlWrapper(ctrl.currentPet));
-
-
 router.post(
   '/pet/add',
   authentication,
@@ -17,7 +15,6 @@ router.post(
   validation(joiSchema),
   ctrlWrapper(ctrl.addPet)
 );
-
 router.delete('/pet/:Id', authentication, ctrlWrapper(ctrl.removePet));
 
 module.exports = router;
